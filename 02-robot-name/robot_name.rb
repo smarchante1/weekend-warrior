57d9a13c75c58cf410e4476cd453179ee653c867
Stephanie Marchante <31323033+smarchante1@users.noreply.github.com>
2019-03-25 15:27:09 -0700

    Update robot_name.rb

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,25 @@
+# robot_name.rb
+
+class Robot 
+  attr_accessor :name
+
+  @old_names = []
+
+  def initialize
+    @name = Robot.create_name
+  end
+
+  def reset
+    @name = Robot.create_name
+  end
+
+  def self.create_name
+    letters = ('A'...'Z').to_a.sample(2).join
+    numbers = rand(100..999)
+    name = "#{letters}#{numbers}"
+    name = create_name if @old_names.include?(name)
+    @old_names << name
+    return name
+  end
+end 
 
