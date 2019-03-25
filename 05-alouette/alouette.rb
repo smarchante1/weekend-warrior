7f9fa141864a6217cb13b0dc9ff3d4508bcf8c72
Stephanie Marchante <31323033+smarchante1@users.noreply.github.com>
2019-03-25 15:04:58 -0700

    Update alouette.rb

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,60 @@
 class Alouette
   def self.lines_for_verse(verse_num)
+    et_lines = ["Et la tête!", "Et le bec!", "Et les yeux!", "Et le cou!", "Et les ailes!", "Et les pattes!", "Et la queue!", "Et le dos!"]
+
+    # return 'Et la tête!' on the very first verse
+    if verse_num == 0
+      return [et_lines[0]]
+    end
+    # correct et line depending on verse number
+    return et_lines[0..verse_num].reverse
   end
 
   def self.verse(verse_num)
+    lines = lines_for_verse(verse_num)
+   
+    lines.map! do |line|
+      line[3..-2]
+    end
+    
+    verse = ""
+
+    2.times do 
+      verse << "Je te plumerai #{lines.first}.\n"
+    end
+    
+    verse = "Je te plumerai #{lines.first}.\nJe te plumerai #{lines.first}."
+
+    lines.each do |line|
+      verse += "\nEt #{line}!\nEt #{line}!"
+    end
+
+    2.times do 
+      verse << "\nAlouette!"
+    end
+
+    verse << "\nA-a-a-ah"
+
+    return verse
   end
 
   def self.sing
+    line = ""
+    8.times do |i|
+      line << "Alouette, gentille alouette,\nAlouette, je te plumerai.\n\n"
+      line << self.verse(i)
+      line << "\n\n"
+    end
+    line << "Alouette, gentille alouette,\nAlouette, je te plumerai."
+    return line
+
+    # verse = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+    # line = ""
+    # 8.times do |i|
+    #   line << verse
+    #   line << ("\n\n" + self.verse(i))
+    #   line << ("\n\n" + verse)
+    # end
+    # return line
   end
 end
