34fe1ec9d08f5440e26842b0bdb0cd422aa2b9d1
Stephanie Marchante <31323033+smarchante1@users.noreply.github.com>
2019-03-25 15:28:28 -0700

    Update word_count.rb

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,10 @@
+def words(phrase)
+    split_phrase = phrase.strip.split(/\s{1,}/)
+    word_count = {}
+    
+    split_phrase.each do |word|
+      word_count[word] = split_phrase.count(word)
+    end
+      return word_count
+end
 
